00ea6efd79e1eec38294a4d6afe18cf9f3fa2b7d
Joao Neves <sevenjp@gmail.com>
2013-04-03 22:39:23 +0200

    Some tiny tweaks and helpers.

--- src/iota.erl
+++ src/iota.erl
@@ -17,16 +17,50 @@ print_help() ->
             "    all   - check for all types of violations~n").
 
 run_check(api, Path) ->
-  scan(Path);
+  do_check([fun verify_api/1], scan(Path));
 run_check(layer, Path) ->
-  scan(Path);
+  do_check([fun verify_layers/1], scan(Path));
 run_check(all, Path) ->
-  scan(Path).
+  do_check([fun verify_api/1, fun verify_layers/1], scan(Path)).
+
+do_check(Checkers, Info) ->
+  lists:foreach(fun(I) ->
+                    lists:foldl(fun(C, Results) ->
+                                    [C(I) | Results]
+                                end, [], Checkers)
+                end, Info).
+
+verify_api({Module, Info}) ->
+  Result = case {get(is_api, Info), get(api, Info)} of
+             {true, []}    -> emit_warning(Module, unrestricted_api);
+             {true, [_|_]} -> ok;
+             {false, _}    -> ok
+           end,
+  {Module, Result}.
+
+verify_layers({_Module, _Info}) ->
+  ok.
+
+emit_warning(ModuleName, Warning) ->
+  io:format("WARNING: ~s~n", [format_warning(ModuleName, Warning)]).
+
+emit_error(ModuleName, Error) ->
+  io:format("ERROR: ~s~n", [format_error(ModuleName, Error)]).
+
+format_warning(ModuleName, unrestricted_api) ->
+  lists:flatten(
+    io_lib:format("Module ~p exports all functions as API functions.",
+                  [ModuleName])).
+
+format_error(_ModuleName, _Error) ->
+  lists:flatten(
+    io_lib:format("", [])).
 
 scan(Path) ->
   LibEbins = filelib:wildcard("ebin", filename:join(Path, "lib")),
   Beams = beams([filename:join(Path, "ebin"), LibEbins]),
-  [io:format("~p: ~p~n", [B, get_iota_data(B)]) || B <- Beams].
+  [{list_to_atom(filename:rootname(filename:basename(B))),
+    get_iota_data(B)} || B <- Beams].
 
 beams(Paths) ->
   lists:foldl(fun(Path, Acc) ->
@@ -40,9 +74,14 @@ get_iota_data(Module) ->
   IotaAttrs = get(iota, Attrs, []),
   Layer = get(layer, IotaAttrs, undefined),
   Api = get(api, Attrs, []),
-  IsApi = get(is_api, IotaAttrs, false) orelse length(Api) > 0,
+  IsApi = get(is_api, IotaAttrs, length(Api) > 0),
   [{layer, Layer}, {is_api, IsApi}, {api, Api}].
 
+get(Key, List) ->
+  case lists:keyfind(Key, 1, List) of
+    false        -> throw(key_not_found);
+    {Key, Value} -> Value
+  end.
 
 get(Key, List, Default) ->
   case lists:keyfind(Key, 1, List) of
