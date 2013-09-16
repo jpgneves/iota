e1c52c5630380e909f00abb4f8c36a62457e3857
Joao Neves <sevenjp@gmail.com>
2013-09-16 18:32:29 +0200

    Add with_xref/1 to utils

--- src/iota_utils.erl
+++ src/iota_utils.erl
@@ -1,7 +1,9 @@
 -module(iota_utils).
 
 -export([ get/2,
-          get/3 ]).
+          get/3,
+          with_xref/1
+        ]).
 
 get(Key, List) ->
   case lists:keyfind(Key, 1, List) of
@@ -13,4 +15,12 @@ get(Key, List, Default) ->
   case lists:keyfind(Key, 1, List) of
     false        -> Default;
     {Key, Value} -> Value
+  end.
+
+with_xref(Fun) ->
+  xref:start(iota),
+  try
+    Fun()
+  after
+    xref:stop(iota)
   end.
---
+++ test/iota_utils_tests.erl
@@ -0,0 +1,14 @@
+-module(iota_utils_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+get_test_() ->
+  [ ?_assertEqual(bar, iota_utils:get(foo, [{foo, bar}])),
+    ?_assertThrow(key_not_found, iota_utils:get(foo, [])),
+    ?_assertEqual(bar, iota_utils:get(foo, [{foo, bar}], 42)),
+    ?_assertEqual(42, iota_utils:get(foo, [], 42))
+  ].
+
+with_xref_test_() ->
+  [ ?_assertEqual(ok, iota_utils:with_xref(fun() -> ok end))
+  ].
