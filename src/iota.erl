600cf9fba837759702b64eaec9894d83efe4c9c4
Joao Neves <sevenjp@gmail.com>
2013-09-16 20:55:13 +0200

    Refactor escript parts

--- src/iota.erl
+++ src/iota.erl
@@ -4,22 +4,23 @@
 
 -spec main([string()]) -> ok | unrecognized_command.
 main([Check, ProjectPath | _]) ->
-  try
-    iota_core:check(list_to_atom(Check), ProjectPath)
-  catch
-    throw:unrecognized_command -> print_help(),
-                                  unrecognized_command
-  end;
+  handle_result(run(Check, ProjectPath));
 main([Check | _]) ->
   {ok, Cwd} = file:get_cwd(),
+  handle_result(run(Check, Cwd));
+main([]) ->
+  print_help().
+
+run(Check, Path) ->
   try
-    iota_core:check(list_to_atom(Check), Cwd)
+    iota_core:check(list_to_atom(Check), Path)
   catch
     throw:unrecognized_command -> print_help(),
                                   unrecognized_command
-  end;
-main([]) ->
-  print_help().
+  end.
+
+handle_result(ok) -> ok;
+handle_result(unrecognized_command) -> halt(1).
 
 print_help() ->
   io:format("usage: iota CHECK [PATH]~n~n  CHECK can be one of the following:~n"
