e6498b931b4cc0a6715adcecbaf94fe5735667f9
Joao Neves <sevenjp@gmail.com>
2013-09-18 15:19:16 +0200

    Add license information and documentation headers in modules

--- src/iota_api_checks.erl
+++ src/iota_api_checks.erl
@@ -1,3 +1,31 @@
+%%% The MIT License (MIT)
+%%%
+%%% Copyright (c) 2013 João Neves
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a
+%%% copy of this software and associated documentation files (the "Software"),
+%%% to deal in the Software without restriction, including without limitation
+%%% the rights to use, copy, modify, merge, publish, distribute, sublicense,
+%%% and/or sell copies of the Software, and to permit persons to whom the
+%%% Software is furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in
+%%% all copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%%% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+%%% DEALINGS IN THE SOFTWARE.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc Implementation of API violation checks
+%%% @author João Neves <sevenjp@gmail.com>
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 -module(iota_api_checks).
 
 -export([ internal_consistency/2,
@@ -54,12 +82,16 @@ verify_external_calls([{Caller, {CalleeM, CalleeF, CalleeA} = Callee}| Rest],
                       {Module, Info}, Results) ->
   NewResults = case iota_utils:get(is_api, Info) of
                  false -> iota_errors:emit_error(Results, Caller,
-                                                 {api, {call_to_non_api_module, CalleeM}});
+                                                 {api,
+                                                  {call_to_non_api_module,
+                                                   CalleeM}});
                  true  -> Api = iota_utils:get(api, Info),
                           case lists:member({CalleeF, CalleeA}, Api) of
-                            false -> iota_errors:emit_error(Results, Caller,
-                                                            {api, {call_to_non_api_function,
-                                                             Callee}});
+                            false ->
+                              iota_errors:emit_error(Results, Caller,
+                                                     {api,
+                                                      {call_to_non_api_function,
+                                                       Callee}});
                             true  -> Results
                           end
                end,
--- src/iota_scanner.erl
+++ src/iota_scanner.erl
@@ -1,3 +1,30 @@
+%%% The MIT License (MIT)
+%%%
+%%% Copyright (c) 2013 João Neves
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a
+%%% copy of this software and associated documentation files (the "Software"),
+%%% to deal in the Software without restriction, including without limitation
+%%% the rights to use, copy, modify, merge, publish, distribute, sublicense,
+%%% and/or sell copies of the Software, and to permit persons to whom the
+%%% Software is furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in
+%%% all copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%%% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+%%% DEALINGS IN THE SOFTWARE.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc Module scanner to extract information required by checks
+%%% @author João Neves <sevenjp@gmail.com>
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 -module(iota_scanner).
 
 -export([ scan/1 ]).
@@ -5,7 +32,8 @@
 scan(Path) ->
   AbsPath = filename:absname(Path),
   Apps = [AbsPath | [filename:join([AbsPath, "lib", L])
-                  || L <- filelib:wildcard("*", filename:join(AbsPath, "lib"))]],
+                     || L <- filelib:wildcard("*",
+                                              filename:join(AbsPath, "lib"))]],
   lists:map(fun(A) ->
                 case xref:add_application(iota_xref, A, [{warnings, false}]) of
                   {ok, _} = R -> R;
